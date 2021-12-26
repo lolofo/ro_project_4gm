e4668dec5188337b012561829c3fe1fdf0b08b6d
lolofo <loicfosse65310@gmail.com>
2021-12-26 18:28:49 +0100

    visu_docx_mip_schrage

--- visu_doc/visu_docx.py
+++ visu_doc/visu_docx.py
@@ -40,29 +40,99 @@ def creat_doc_data(N,a,d,q) :
     return doc
 
 def doc_add_shrage_sol(N,a,d,q):
-
-    plt.savefig('client_report/test.png')
-
+    '''
+    add to the previous document, the solution obtained with schrage heurisitc
+    '''
     document = creat_doc_data(N,a,d,q)
-    document.add_heading('Report',0)
-    document.add_picture('test.png', width=Inches(1.25))
+    
+    document.add_page_break()
+    document.add_heading('Solve with the Schrage Heuristic', 0)
+    solution , fig = solve_schrage_heuristic(N,a,d,q , show_output = False ,graphics = False , larg = 10 , haut = 10)
+    schedule = ["Job "+str(i) for i in solution['SCHD']]
+    value = solution['UB']
+    run_time = solution['time']
 
-def doc_add_mip_sol(doc):
-    pass
+    # creation of the table of the solution
+    document.add_heading('The raw data : ', 1)
+    table = document.add_table(rows=1, cols=2)
 
-def doc_compute_save(N,a,d,q,name="test.docx"):
-    pass
+    
+
+    row = table.rows[0].cells
+
+    row = table.add_row().cells
+    row[0].text = 'Total time'
+    row[1].text = str(value)
+
+    row = table.add_row().cells
+    row[0].text = "Schedule"
+    row[1].text = str(schedule)
+
+    row = table.add_row().cells
+    row[0].text = "Running time"
+    row[1].text = str(np.round(run_time,3))
+    
+    table.style = 'Colorful List'
+
+    document.add_heading('The real Schedule', 1)
+    fig = visu_calender(N,a,d,q,solution['SCHD'])
+
+    plt.savefig('client_report/buff.png')
+    document.add_picture('client_report/buff.png', width=Inches(7))
+
+    return document
+
+def doc_add_mip_sol(N,a,d,q):
+    '''
+    add to the previous document the solution with the mip
+    '''
+    document = doc_add_shrage_sol(N,a,d,q)
+    
+    document.add_page_break()
+    document.add_heading('Solve with the Pulp Solver', 0)
+    solution = solve_pulp_model(N,a,d,q , show_output = False)
+    schedule = ["Job "+str(i) for i in solution['SCHD']]
+    value = solution['OBJ']
+    run_time = solution['RUN_TIME']
+
+    # creation of the table of the solution
+    document.add_heading('The raw data : ', 1)
+    table = document.add_table(rows=1, cols=2)
+
+    
+
+    row = table.rows[0].cells
+
+    row = table.add_row().cells
+    row[0].text = 'Total time'
+    row[1].text = str(value)
+
+    row = table.add_row().cells
+    row[0].text = "Schedule"
+    row[1].text = str(schedule)
+
+    row = table.add_row().cells
+    row[0].text = "Running time"
+    row[1].text = str(np.round(run_time,3))
+    
+    table.style = 'Colorful List'
+
+    document.add_heading('The real Schedule', 1)
+    fig = visu_calender(N,a,d,q,solution['SCHD'])
+
+    plt.savefig('client_report/buff.png')
+    document.add_picture('client_report/buff.png', width=Inches(7))
+
+    return document
 
 
 
-
-
-
-if __name__ == "__main__" :
-
-    N = list(range(1,8))
-    a = [10,13,11,20,30,0,30]
-    d = [5,6,7,4,3,6,2]
-    q = [7,26,24,21,8,17,0]
-
-    pass
+def create_save_doc(N,a,d,q , name = "default") :
+    '''
+    input : N, a, d, q --> data of the problem
+            name       --> name of the document (without the .docx)
+    '''
+    name += ".docx" # --> docx format for the document
+    doc = doc_add_mip_sol(N,a,d,q)
+    doc.save("client_report/"+name)
+    
